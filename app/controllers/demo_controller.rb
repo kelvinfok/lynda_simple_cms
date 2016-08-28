201f2f965e318dbb19305c6a53fe16b4883037f5
messyhat <punkd1+1@gmail.com>
2016-08-29 00:08:22 +0800

    Chapter 10

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -9,7 +9,7 @@ class DemoController < ApplicationController
     # render('index')
     @array = [1,2,3,4,5]
     @id = params[:id].to_i
-    @page = params[:page].to_i 
+    @page = params[:page].to_i
   end
 
   def other_hello
@@ -18,6 +18,9 @@ class DemoController < ApplicationController
 
   def lynda
     redirect_to("http://lynda.com")
+  end
+
+  def text_helpers
   end
 
 end
